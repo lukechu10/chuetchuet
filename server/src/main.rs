95f0822413de50cfbf8c979dcaaf6a68109c5b5f
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-05-17 11:23:23 -0700

    Add some dependencies

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,18 @@
-use rocket::launch;
+use rocket::{catch, catchers, get, launch, routes};
+
+#[catch(404)]
+fn not_found() -> &'static str {
+    "404 Not Found"
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Terra Noun API"
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .mount("/", routes![index])
+        .register("/", catchers![not_found])
 }
