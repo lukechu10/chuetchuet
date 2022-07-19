7d7208938be8bed52d7dfc5e39bd745ea5294428
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-07-19 15:17:01 -0700

    Add react native navigation

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,16 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>Chuetchuet</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <StatusBar />
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View>
+            <Text>Chuetchuet</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
